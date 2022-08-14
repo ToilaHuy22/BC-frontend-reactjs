50231cdff0ef291480f12b09fc3e7c60cc7d5c17
Huy <xinhtupeanh@gmail.com>
2022-08-14 23:39:07 +0700

    configuration app and create login form

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,123 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { push } from "connected-react-router";
+
+import * as actions from "../../store/actions";
+import "./Login.scss";
+import { FormattedMessage } from "react-intl";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      isShowPassword: false,
+    };
+  }
+
+  handleOnChangeUserName = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+  handleOnChangePassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  hanldeLogin = () => {
+    console.log(this.state.username);
+    console.log(this.state.password);
+  };
+
+  handleShowHidePassword = () => {
+    this.setState({
+      isShowPassword: !this.state.isShowPassword,
+    });
+  };
+
+  render() {
+    return (
+      <div className="login-background">
+        <div className="login-container">
+          <div className="login-content row">
+            <div className="col-12 login-text">Login</div>
+            <div className="col-12 form-group login-input">
+              <label htmlFor="">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your username"
+                value={this.state.username}
+                onChange={(event) => this.handleOnChangeUserName(event)}
+              />
+            </div>
+            <div className="col-12 form-group login-input">
+              <label htmlFor="">Password</label>
+              <div className="custom-input-password">
+                <input
+                  type={this.state.isShowPassword ? "text" : "password"}
+                  className="form-control"
+                  placeholder="Enter your password"
+                  value={this.state.password}
+                  onChange={(event) => this.handleOnChangePassword(event)}
+                />
+                <span
+                  onClick={() => {
+                    this.handleShowHidePassword();
+                  }}
+                >
+                  <i
+                    class={
+                      this.state.isShowPassword
+                        ? "fas fa-eye"
+                        : "fas fa-eye-slash"
+                    }
+                  ></i>
+                </span>
+              </div>
+            </div>
+            <div className="col-12">
+              <button
+                className="btn-login mt-1"
+                onClick={() => this.hanldeLogin()}
+              >
+                Login
+              </button>
+            </div>
+            <div className="col-12">
+              <span className="forgot-password">Forgot your password?</span>
+            </div>
+            <div className="col-12 text-center mt-3">
+              <span className="text-other-login">Or Login with:</span>
+            </div>
+            <div className="col-12 social-login mt-1">
+              <i class="fab fa-google-plus-square"></i>
+              <i class="fab fa-facebook"></i>
+              <i class="fab fa-twitter-square"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    navigate: (path) => dispatch(push(path)),
+    adminLoginSuccess: (adminInfo) =>
+      dispatch(actions.adminLoginSuccess(adminInfo)),
+    adminLoginFail: () => dispatch(actions.adminLoginFail()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
