db5913dcbf95e0597a74d376e040b328bb533664
Huy <xinhtupeanh@gmail.com>
2022-08-26 19:20:54 +0700

    Redux (Design) create form {add user redux} and create translation

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -244,6 +244,7 @@ class HomeHeader extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    // userInfo: state.user.userInfo,
     language: state.app.language,
   };
 };
