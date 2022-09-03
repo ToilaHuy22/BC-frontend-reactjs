8c53fcdc59b3f3f4a76667937c25c8e19fe2e976
Huy <xinhtupeanh@gmail.com>
2022-09-03 11:28:00 +0700

    38 Install, run React-markdown-editor-lite and run Markdown-it

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -4,6 +4,22 @@ import { connect } from "react-redux";
 import "./TableManageUser.scss";
 import * as actions from "../../../store/actions";
 
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+// import style manually
+import "react-markdown-editor-lite/lib/index.css";
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+  console.log("handleEditorChange", html, text);
+}
+
 class TableManageUser extends Component {
   constructor(props) {
     super(props);
@@ -83,6 +99,11 @@ class TableManageUser extends Component {
               })}
           </tbody>
         </table>
+        <MdEditor
+          style={{ height: "500px" }}
+          renderHTML={(text) => mdParser.render(text)}
+          onChange={handleEditorChange}
+        />
       </div>
     );
   }
