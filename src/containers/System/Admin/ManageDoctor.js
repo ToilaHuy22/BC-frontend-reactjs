6336dce497d325831bf9adc4b6b7ea88395e7ff1
Huy <xinhtupeanh@gmail.com>
2022-09-03 11:40:21 +0700

    39 Create new Route ManageDoctor (Doctor Detail)

---
+++ src/containers/System/Admin/ManageDoctor.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./TableManageUser.scss";
+import * as actions from "../../../store/actions";
+
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+// import style manually
+import "react-markdown-editor-lite/lib/index.css";
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+  console.log("handleEditorChange", html, text);
+}
+
+class ManageDoctor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usersRedux: [],
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchUserRedux();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.listUsers !== this.props.listUsers) {
+      this.setState({
+        usersRedux: this.props.listUsers,
+      });
+    }
+  }
+
+  handleDeleteUser = (user) => {
+    this.props.deleteAUserRedux(user.id);
+  };
+
+  handleEditUser = (user) => {
+    this.props.handleEditUserFromParent(user);
+  };
+
+  render() {
+    return (
+      <div className="my-5">
+        <div className="">Tạo thêm thông tin Bác sĩ</div>
+        <MdEditor
+          style={{ height: "500px" }}
+          renderHTML={(text) => mdParser.render(text)}
+          onChange={handleEditorChange}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    listUsers: state.admin.users,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUserRedux: () => dispatch(actions.fetchAllUsersStart()),
+    deleteAUserRedux: (id) => dispatch(actions.deleteAUser(id)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageDoctor);
